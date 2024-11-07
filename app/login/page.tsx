b9178a3cbb985252f7db78faab0d68e758051a80
henriquegomesteixeira <henriquegomes080012@gmail.com>
2024-11-06 20:29:31 -0400

    feat: add login page

---
+++ app/login/page.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import React from "react";
+import { Button } from "../_components/ui/button";
+import { LogIn } from "lucide-react";
+
+function LoginPage() {
+  return (
+    <div className="grid h-full items-center lg:grid-cols-2">
+      <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
+        <Image
+          src="/logo.svg"
+          alt="Finance AI"
+          width={173}
+          height={39}
+          className="mb-8"
+        />
+        <h1 className="font mb-3 text-4xl font-bold">Bem-vindo</h1>
+        <p className="mb-8 text-muted-foreground">
+          A Finance AI é uma plataforma de gestão financeira que utiliza IA para
+          monitorar suas movimentações, e oferecer insights personalizados,
+          facilitando o controle do seu orçamento.
+        </p>
+        <Button variant="outline">
+          <LogIn className="mr-2" />
+          Fazer login ou criar conta
+        </Button>
+      </div>
+      <div className="mx-[10%] hidden grid-rows-[70vh_15%] gap-y-10 lg:grid xl:mx-[15%] 2xl:mx-[25%]">
+        <div className="relative">
+          <Image
+            src="/login.png"
+            alt="Faça login"
+            fill
+            priority
+            sizes="100vw"
+            className="rounded-2xl object-cover object-left-top"
+          />
+        </div>
+        <div className="flex flex-col justify-center rounded-xl">
+          <Image src="/logo.svg" alt="Finance AI" width={173} height={39} />
+          <p>
+            Gerencie suas finanças pessoais de uma forma simples com a
+            finance.ai, e o melhor, totalmente de graça!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
