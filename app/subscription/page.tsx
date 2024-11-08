6a6742d67193fd6915c75bebbe2ca2656e8d5f90
henriquegomesteixeira <henriquegomes080012@gmail.com>
2024-11-08 16:24:16 -0400

    feat: add navbar component

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,11 +1,8 @@
-import React from 'react';
+import React from "react";
+import Navbar from "../_components/navbar";
 
 function SubscriptionPage() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <Navbar />;
 }
 
 export default SubscriptionPage;
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -2,22 +2,26 @@ import { db } from "../_lib/prisma";
 import { DataTable } from "../_components/ui/data-table";
 import { TransactionColumns } from "./_columns";
 import AddTransactionButton from "../_components/add-transaction-button";
+import Navbar from "../_components/navbar";
 
 const TransactionsPage = async () => {
   // Busca todas as transações
   const transactions = await db.transaction.findMany({});
 
   return (
-    <div className="space-y-6 p-6">
-      <div className="flex w-full items-center justify-between">
-        <h1 className="text-2xl font-bold">Transações</h1>
-        <AddTransactionButton />
+    <>
+      <Navbar />
+      <div className="space-y-6 p-6">
+        <div className="flex w-full items-center justify-between">
+          <h1 className="text-2xl font-bold">Transações</h1>
+          <AddTransactionButton />
+        </div>
+        <DataTable
+          columns={TransactionColumns}
+          data={JSON.parse(JSON.stringify(transactions))}
+        />
       </div>
-      <DataTable
-        columns={TransactionColumns}
-        data={JSON.parse(JSON.stringify(transactions))}
-      />
-    </div>
+    </>
   );
 };
 
