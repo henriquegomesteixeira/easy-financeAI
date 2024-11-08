00173ac62f134af76a2dd2a3d40a3fa6b56f3855
henriquegomesteixeira <henriquegomes080012@gmail.com>
2024-11-08 13:50:03 -0400

    feat: serialize transactions data for DataTable

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -13,7 +13,10 @@ const TransactionsPage = async () => {
         <h1 className="text-2xl font-bold">Transações</h1>
         <AddTransactionButton />
       </div>
-      <DataTable columns={TransactionColumns} data={transactions} />
+      <DataTable
+        columns={TransactionColumns}
+        data={JSON.parse(JSON.stringify(transactions))}
+      />
     </div>
   );
 };
